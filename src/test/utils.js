a92009c649c42e27160d23699f07301115289323
Ben Warzeski <bwarzeski@edx.org>
2021-10-26 16:41:02 -0400

    fix: remove un-necessary utils

--- src/test/utils.js
+++
@@ -1,7 +0,0 @@
-export const mockSuccess = (returnValFn) => (...args) => (
-  new Promise((resolve) => resolve(returnValFn(...args)))
-);
-
-export const mockFailure = (returnValFn) => (...args) => (
-  new Promise((resolve, reject) => reject(returnValFn(...args)))
-);
